c360c10d14d09fd039209ef3af86e19c881b3ef8
Allen Yang <155710586+defilement@users.noreply.github.com>
2025-04-21 20:48:42 -0700

    OUR FULL PA9
    download into a folder name "Project" with sfml 3.0 inside of it . report any bugs to me

---
+++ SFMLTesting/SFMLTesting/Game.hpp
@@ -0,0 +1,32 @@
+#include "ObjectManager.hpp"
+
+/*
+Game wrapper hpp class
+Displays Main menu, game screen, and game over screen
+Contains the lives, combo, points and high score for the current game
+*/
+
+class Game
+{
+public:
+	Game(); // constructor
+
+	// 3 Window states 
+	bool mainMenu();
+	void runGame();
+	void gameOver();
+
+	// Helpers
+	void respawnFruit(std::vector<std::unique_ptr<ObjectManager>>& fruitObjects); 
+
+
+private:
+	// Data members 
+	int mLives;
+	int mCombo;
+	int mPoints;
+	int mHighScore;
+
+	// Current clock to spawn fruits
+	sf::Clock fruitClock;
+};
---
+++ SFMLTesting/SFMLTesting/README.txt
@@ -0,0 +1,13 @@
+CPTS_122 PA9
+Allen Yang : 11902643
+Faisal Kherwish : 11879926
+
+
+Fruit Slicers :
+
+A homemade infinitely progressive fruit slicing game inspired by the classical Fruit Ninja. 
+Every minute the fruits will spawn faster and faster.
+Watch out past 7 minutes, the game will get intensely more difficult!
+Slice the fruits, avoid the bombs, reach the highest score possible!
+
+Fanmade project, no relation to Fruit Ninja or Halfbrick
